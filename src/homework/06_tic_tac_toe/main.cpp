f164381f1e1931f76e06e40a619f8ba4c3c34982
Geoffrey K <geoffrykrishnan@gmail.com>
2025-11-09 20:45:36 +0000

    Completed HW 7

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -7,21 +7,66 @@ using std::string;
 
 int main() {
 	TicTacToe tic_tac_toe;
+	string user_input = "";
 
-	string first_player = "X";
+	do {
+		string first_player = "";
 
-	cout << "Enter first player (X or O): ";
-	cin >> first_player;
+		do {
+			cout << "Enter first player (X or O): ";
+			cin >> first_player;
 
-	tic_tac_toe.start_game(first_player);
+			if(first_player != "X" && first_player != "O") {
+				cout << "Invalid input. Please enter X or O.\n";
+			}
+		} while(first_player != "X" && first_player != "O");
 
-	while(!tic_tac_toe.game_over()) {
-		auto position = 0;
-		cout<<"Enter position: ";
-		cin>>position;
+		tic_tac_toe.start_game(first_player);
 
-		tic_tac_toe.mark_board(position);
-		tic_tac_toe.display_board();
-	}
+		while(!tic_tac_toe.game_over()) {
+			auto position = 0;
+			bool valid_move = false;
+
+			do {
+				cout << "Enter position (1-9): ";
+				cin >> position;
+
+				if(cin.fail()) {
+					cin.clear();
+					cin.ignore(1000, '\n');
+					cout << "Invalid input. Please enter a number.\n";
+					valid_move = false;
+				}
+				else if(position < 1 || position > 9) {
+					cout << "Invalid position. Please enter a number between 1 and 9.\n";
+					valid_move = false;
+				}
+				else if(!tic_tac_toe.is_position_available(position)) {
+					cout << "Position already taken. Please choose another position.\n";
+					valid_move = false;
+				}
+				else {
+					valid_move = true;
+				}
+			} while(!valid_move);
+
+			tic_tac_toe.mark_board(position);
+			tic_tac_toe.display_board();
+		}
+
+		if(tic_tac_toe.get_winner() == "C") {
+			cout << "Game is a tie!\n";
+		}
+		else {
+			cout << "Winner is: " << tic_tac_toe.get_winner() << "\n";
+		}
+
+		cout << "Play again? (y/n or q to quit): ";
+		cin >> user_input;
+
+	} while(user_input != "n" && user_input != "q");
+
+	cout << "Thanks for playing!\n";
+
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -14,15 +14,22 @@ class TicTacToe {
         void start_game(string firstPlayer);
         void mark_board(int position);
         void display_board() const;
+        bool is_position_available(int position) const;
 
         string get_player() const {return player;}
+        string get_winner() const {return winner;}
 
     private:
         void set_next_player();
         bool check_board_full();
         void clear_board();
+        bool check_column_win();
+        bool check_row_win();
+        bool check_diagonal_win();
+        void set_winner();
 
         string player{};
+        string winner{};
         vector<string> pegs{9, " "};
 };
 
